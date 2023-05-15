194887be049a1359fb3bc1f7c3e92b562fbb9446
praveesh hassan <praveesh@yahoo.com>
2023-05-15 19:25:24 +0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Praveesh Hassan on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
